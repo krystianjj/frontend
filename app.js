658993dcbf81cd1513e829316819e6175735fa6a
Krystian <krystianjj@gmail.com>
2016-04-08 11:56:03 +0200

    working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var myapp = angular.module('sampleapp', [ 'ngAnimate', 'ui.bootstrap' ]);
-myapp.controller('samplecontoller', function ($scope, $http) {
+myapp.controller('samplecontoller', function ($scope, $http, $uibModal) {
 // GET DATA FROM API
 $scope.showData = function( $parametr ){
     $http.get("http://test-api.kuria.tshdev.io/?" + $parametr ).success(function(response) {  
